6d2979f3c5e2e7f97125b12e49043958446cae6c
Tegan Sims <tegansims@hotmail.com>
2019-06-17 18:24:46 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   x = 0 
   new_array = []
-  while counter < array.length 
+  while x < array.length 
     new_array << array[x] * array[x]
     x +=1 
   end
